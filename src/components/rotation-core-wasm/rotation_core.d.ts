b988def43a49ee83c8fe5f2aad26d192200f2f57
Seihposwag <angelsexyswag2004@gmail.com>
2026-06-15 01:19:36 +0300

    fix: wasm component

---
+++ src/components/rotation-core-wasm/rotation_core.d.ts
@@ -0,0 +1,39 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function rot_by_rotmat_inplace(vertices: Float32Array, angle: number, axis: number): void;
+
+export function rotate_vertices_axis_angle_inplace(vertices: Float32Array, ax: number, ay: number, az: number, angle: number): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly rot_by_rotmat_inplace: (a: number, b: number, c: any, d: number, e: number) => void;
+    readonly rotate_vertices_axis_angle_inplace: (a: number, b: number, c: any, d: number, e: number, f: number, g: number) => void;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ src/components/rotation-core-wasm/rotation_core_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const rot_by_rotmat_inplace: (a: number, b: number, c: any, d: number, e: number) => void;
+export const rotate_vertices_axis_angle_inplace: (a: number, b: number, c: any, d: number, e: number, f: number, g: number) => void;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
